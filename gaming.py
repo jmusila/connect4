c214713526c6144121b28247f403dfd9ca793004
jonathanmusila <jonathanmusila6@gmail.com>
2019-07-06 07:24:08 +0300

    added main functions

---
+++ gaming.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+def drop_piece():
+    pass
+
+def is_valif_location():
+    pass
+
+def get_next_open_row():
+    pass
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 Input
+    if turn == 0:
+        selection = int(input("Player 1 Make your Selection (0-6):"))
+
+
+    # Ask for Player 2 Input
+    else:
+        selection = int(input("Player 2 Make your Selection (0-6):"))
+
+    turn +=1
+    turn = turn % 2
+
